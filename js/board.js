afd81effee05de1a806bd034eab1a0cd8be2dc4f
Hardavan Alaie <h.alaie@gmx.de>
2025-01-12 20:44:21 +0100

    Board-page

--- js/board.js
+++ js/board.js
@@ -1,47 +1,53 @@
-let todos = [{
+let todos = [
+  {
     id: 1,
     title: "Task 1",
     status: "To do",
     priority: "Low",
-    category: "To do"
-},{
+    category: "To do",
+  },
+  {
     id: 2,
     title: "Task 2",
     status: "In progress",
     priority: "Low",
-    category: "In progress"
-},{
+    category: "In progress",
+  },
+  {
     id: 3,
     title: "Task 3",
     status: "To do",
     priority: "Low",
-    category: "Await feedback"
-},{
+    category: "Await feedback",
+  },
+  {
     id: 4,
     title: "Task 4",
     status: "To do",
     priority: "Low",
-    category: "Done"
-}
-]
-
+    category: "Done",
+  },
+];
 
 let currentDraggedElement;
 let currentSelectedTask;
 
 function renderTaskContainer() {
-    //let categories = todos.filter(c => c['category'] == 'To do', 'In progress', 'Await feedback', 'Done');
-    //let categories = document.getElementById('taskContainer');
-    //document.getElementById('taskContainer').innerHTML = '';
+  //let categories = todos.filter(c => c['category'] == 'To do', 'In progress', 'Await feedback', 'Done');
+  //let categories = document.getElementById('taskContainer');
+  //document.getElementById('taskContainer').innerHTML = '';
 
-    //let currentDraggedElement = categories;
+  //let currentDraggedElement = categories;
 
-    
+  let categories = todos.map((c) => c["category"]);
+  let uniqueCategories = [...new Set(categories)];
 
-    for (let index = 0; index < todos.length; index++) {
-        const element = todos[index];
-        document.getElementById('taskContainer').innerHTML += /*html*/`
-        <div class="task-card" id="taskCardToDo${element.category}" draggable="true" ondragstart="startDragging('taskCardToDo${element.category}')" ondragend="removeHighlight('To do')" ondrop="moveTo('In progress')" ondragleave="removeHighlight('In progress')" ondragover="allowDrop(event); highlight('In progress')">
+  for (let index = 0; index < uniqueCategories.length; index++) {
+    const element = uniqueCategories[index];
+    document.getElementById("taskContainer").innerHTML += /*html*/ `
+        <div class="column" id="${element}">
+            <h2 class="column-titles-h2">${element}<button class="add-column"><img src="/assets/icons/plusblack.png" alt=""></button></h2>
+        <div class="task-card" id="taskCardToDo${element.category}" draggable="true" ondragstart="startDragging('taskCardToDo${element.category}')" ondragend="removeHighlight('To do')" ondrop=" moveTo('To do') moveTo('In progress') moveTo('Await feedback') moveTo('Done')" ondragleave="removeHighlight('To do') removeHighlight('In progress') removeHighlight('Await feedback') removeHighlight('Done')" ondragover="allowDrop(event); highlight('In progress')">
             <div class="task-content">
                 <div class="task-card-title">${element.title}</div>
                 <h3 class="task-title">${element.title}</h3>
@@ -64,35 +70,36 @@ function renderTaskContainer() {
                 </div>
             </div>
         </div>
+        </div>
         `;
-    }
+  }
 }
 
 function startDragging(category) {
-    currentDraggedElement = category;
+  currentDraggedElement = category;
 }
 
 function allowDrop(ev) {
-    ev.preventDefault();
+  ev.preventDefault();
 }
 
 function moveTo(category) {
-    todos[currentDraggedElement]['category'] = category;
-    renderTaskContainer();
+  todos[currentDraggedElement]["category"] = category;
+  renderTaskContainer();
 }
 
 function highlight(category) {
-    document.getElementById(category).classList.add('drag-area-highlight');
+  document.getElementById(category).classList.add("drag-area-highlight");
 }
 
 function removeHighlight(category) {
-    document.getElementById(category).classList.remove('drag-area-highlight');
+  document.getElementById(category).classList.remove("drag-area-highlight");
 }
 
 function updateProgressBar() {
-    let update = (currentSelectedTask + 1) / todos.length;
-    
-    update = Math.round(update * 100);
-    document.getElementById('progressbar').innerHTML = `${update} %`;
-    document.getElementById('progressbar').style = `width: ${update}%;`;
+  let update = (currentSelectedTask + 1) / todos.length;
+
+  update = Math.round(update * 100);
+  document.getElementById("progressbar").innerHTML = `${update} %`;
+  document.getElementById("progressbar").style = `width: ${update}%;`;
 }
