f081b3abeb162f45303678ea6a174f97ef55b33a
Hardavan Alaie <h.alaie@gmx.de>
2025-01-22 08:42:33 +0100

    login-test

--- js/logIn.js
+++ js/logIn.js
@@ -1,47 +1,123 @@
-let users = [
+/*let users = [
 	{
 		name: "Max Muster",
 		initial: "MM",
-		email: "max@mm.com",
-		password: 123,
+		'email': "max@mm.com",
+		'password': 123,
 		phone: "01234567890",
 	},
 	{
 		name: "Dax Duster",
 		initial: "DD",
-		email: "dax@mm.com",
-		password: 123,
+		'email': "Dax@mm.com",
+		'password': 123,
 		phone: "1234567890",
 	},
 	{
 		name: "Fax Fuster",
 		initial: "FF",
-		email: "fax@mm.com",
-		password: 123,
+		'email': "fax@mm.com",
+		'password': 123,
 		phone: "234567890",
 	},
-];
+];*/
+
+//let BASE_URL = 'https://join-7f1d9-default-rtdb.europe-west1.firebasedatabase.app/';
+
+let users = [];
+
+
+
+async function loadData(path = "") {
+	let response = await fetch(BASE_URL + path + ".json");
+	let responseToJson = await response.json();
+	console.log(responseToJson);
+
+	users = responseToJson;
+
+
+	let email = document.getElementById("loginEmail").value;
+	let password = document.getElementById("loginPassword").value;
+
+	users.push();
+	console.log(users);
+
+
+	let user = users.find(user => user.email === email && user.password === password);
+
+
+	//let user = users.find(user => user.email === email.value && user.password === password.value);
+
+
+	console.log(user);
+
+	if (user) {
+		console.log("Anmeldung erfolgreich");
+	}
+
+}
+
+
+
+/*function init() {
+	logIn("");
+}*/
+
+/*async function logIn(path = 'signup["-OH2GD62vDxlyL1sFlFh"]') {
+	let response = await fetch(BASE_URL + path + '.json');
+	let responseToJson = await response.json();
+	console.log(responseToJson);
+
+	let email = document.getElementById("loginEmail").value;
+	let password = document.getElementById("loginPassword").value;
+
+	if (email == email && password == password) {
+		console.log("Anmeldung erfolgreich");
+		
+	}
+
+}*/
+
+/*async function dataLogin(path = '') {
+	let response = await fetch(BASE_URL + path + '.json');
+	let responseToJson = await response.json();
+	console.log(responseToJson);
+	
+	let email = document.getElementById("loginEmail");
+	let password = document.getElementById("loginPassword");
+	
+	let users = responseToJson;
+	let user = users.find(user => user.email == email.value && user.password == password.value);
+		
+	
+	console.log(users);
+	console.log(user);
+
+	
+}*/
+
 
 function guestLogin() {
 	sessionStorage.setItem("username", "Guest");
 	window.location.href = "../pages/bord.html";
 }
 
-function logIn() {
+/*function logIn() {
 	let email = document.getElementById("loginEmail");
 	let password = document.getElementById("loginPassword");
 
-	let name = users.find(
-		(name) => name.email === email.value && name.password === password.value
-	);
-	console.log(name);
 
-	if (name) {
+	let user = users.find(
+		user => user.email == email.value && user.password == password.value
+	);
+	console.log(user);
+
+	if (user) {
 		console.log("Anmeldung");
 	} else {
 		console.log("Fehler bei der Anmeldung");
 	}
-}
+}*/
 
 // async function logIn(path = '') {
 //   // let BASE_URL = 'https://join-7f1d9-default-rtdb.europe-west1.firebasedatabase.app/';
--- js/signUp.js
+++ js/signUp.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://join-7f1d9-default-rtdb.europe-west1.firebasedatabase.app/';
+const BASE_URL = "https://join-7f1d9-default-rtdb.europe-west1.firebasedatabase.app/";
 
 async function init() {
   onloadFunc();
