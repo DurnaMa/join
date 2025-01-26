c288bf27ca8c1dc911df9a4d2ea3b2e32c982750
Hardavan Alaie <h.alaie@gmx.de>
2025-01-26 09:58:48 +0100

    board page task cards functions

--- js/board.js
+++ js/board.js
@@ -1,38 +1,47 @@
 let todos = [
   {
     id: 1,
-    title: 'Task 1',
-    category: 'To do',
+    title: "Task 1",
+    category: "To do",
   },
   {
     id: 2,
-    title: 'Task 2',
-    category: 'In progress',
+    title: "Task 2",
+    category: "In progress",
   },
   {
     id: 3,
-    title: 'Task 3',
-    category: 'Await feedback',
+    title: "Task 3",
+    category: "Await feedback",
   },
   {
     id: 4,
-    title: 'Task 4',
-    category: 'Done',
+    title: "Task 4",
+    category: "Done",
   },
 ];
+
 
 let currentDraggedElement;
 let currentSelectedTask;
 
-
 function updateHTML() {
-  let categories = todos.filter(c => c['category'] == 'To do' || c['category'] == 'In progress' || c['category'] == 'Await feedback' || c['category'] == 'Done');
+  let categories = todos.filter(
+    (c) =>
+      c["category"] == "To do" ||
+      c["category"] == "In progress" ||
+      c["category"] == "Await feedback" ||
+      c["category"] == "Done"
+  );
 
-  document.getElementById('columnContainer').innerHTML = '';
+  document.getElementById("columnContainer").innerHTML = "";
 
   for (let index = 0; index < categories.length; index++) {
     const element = categories[index];
-    document.getElementById('columnContainer').innerHTML += renderTaskContainer(element['category']);
+    document.getElementById("columnContainer").innerHTML += renderTaskContainer(
+      //element,
+      element["category"]
+    );
   }
 }
 
@@ -44,7 +53,12 @@ function renderTaskContainer(element) {
   return /*html*/ `
     <div class="column">
         <h2 class="column-titles-h2">${element}<button class="add-column"><img src="/assets/icons/plusblack.png" alt=""></button></h2>
-        <div class="column-content" ondragstart="startDragging(${element['id']})">${element['title']}</div>
+        <div class="task-card" id="${element}" draggable="true" ondragstart="startDragging(${element["id"]})">
+        <div class="task-content">
+            <div class="task-card-title">${element["title"]}</div>
+            <h3 class="task-title">${element.title}</h3>
+            <p class="task-description">${element.description}</p>
+        </div>
     </div>
   `;
 }
@@ -54,11 +68,45 @@ function allowDrop(ev) {
 }
 
 function moveTo(category) {
-  todos[currentDraggedElement]['category'] = category;
+  todos[currentDraggedElement]["category"] = category;
   updateHTML();
 }
 
 
+/*function updateHTML() {
+  let todo = todos.filter((c) => c["category"] == "To do");
 
+  document.getElementById("columnContainer").innerHTML = "";
 
+  for (let index = 0; index < todo.length; index++) {
+    const element = todo[index];
+    document.getElementById("columnContainer").innerHTML += renderTaskContainer(element);
+  }
 
+  let inProgress = todos.filter((c) => c["category"] == "In progress");
+
+  document.getElementById("columnContainer").innerHTML = "";
+
+  for (let index = 0; index < inProgress.length; index++) {
+    const element = inProgress[index];
+    document.getElementById("columnContainer").innerHTML += renderTaskContainer(element);
+  }
+
+  let awaitFeedback = todos.filter((c) => c["category"] == "Await feedback");
+
+  document.getElementById("columnContainer").innerHTML = "";
+
+  for (let index = 0; index < awaitFeedback.length; index++) {
+    const element = awaitFeedback[index];
+    document.getElementById("columnContainer").innerHTML += renderTaskContainer(element);
+  }
+
+  let done = todos.filter((c) => c["category"] == "Done");
+
+  document.getElementById("columnContainer").innerHTML = "";
+
+  for (let index = 0; index < done.length; index++) {
+    const element = done[index];
+    document.getElementById("columnContainer").innerHTML += renderTaskContainer(element);
+  }
+}*/
