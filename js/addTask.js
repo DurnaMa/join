3b40d35bea46a637c30d987a554d1d8999b58ab7
Mahir Durna <159443825+DurnaMa@users.noreply.github.com>
2025-02-05 18:01:01 +0100

    contackts in addTaske Assigned to

--- js/addTask.js
+++ js/addTask.js
@@ -1,53 +1,58 @@
-let subTask = document.getElementById('subTask');
+async function initAddTask() {
+  await loadDataUsers();
+  contactList();
+}
+
+let subTask = document.getElementById("subTask");
 let subTasks = [];
 
 function prioUrgent() {
-  document.getElementById('prioUrgentEdit').classList.add('prioUrgentRed');
-  document.getElementById('urgentImg').src = '/assets/icons/urgentWhite.png';
-  document.getElementById('prioMediumEdit').classList.remove('prioMediumYellow');
-  document.getElementById('prioLowEdit').classList.remove('prioLowGreen');
-  document.getElementById('mediumImg').src = '/assets/icons/mediumYellow.png';
-  document.getElementById('lowImg').src = '/assets/icons/lowGreen.png';
+  document.getElementById("prioUrgentEdit").classList.add("prioUrgentRed");
+  document.getElementById("urgentImg").src = "/assets/icons/urgentWhite.png";
+  document.getElementById("prioMediumEdit").classList.remove("prioMediumYellow");
+  document.getElementById("prioLowEdit").classList.remove("prioLowGreen");
+  document.getElementById("mediumImg").src = "/assets/icons/mediumYellow.png";
+  document.getElementById("lowImg").src = "/assets/icons/lowGreen.png";
 }
 function prioMedium() {
-  document.getElementById('prioMediumEdit').classList.add('prioMediumYellow');
-  document.getElementById('mediumImg').src = '/assets/icons/mediumWhite.png';
-  document.getElementById('prioLowEdit').classList.remove('prioLowGreen');
-  document.getElementById('prioUrgentEdit').classList.remove('prioUrgentRed');
-  document.getElementById('lowImg').src = '/assets/icons/lowGreen.png';
-  document.getElementById('urgentImg').src = '/assets/icons/urgentRed.png';
+  document.getElementById("prioMediumEdit").classList.add("prioMediumYellow");
+  document.getElementById("mediumImg").src = "/assets/icons/mediumWhite.png";
+  document.getElementById("prioLowEdit").classList.remove("prioLowGreen");
+  document.getElementById("prioUrgentEdit").classList.remove("prioUrgentRed");
+  document.getElementById("lowImg").src = "/assets/icons/lowGreen.png";
+  document.getElementById("urgentImg").src = "/assets/icons/urgentRed.png";
 }
 function prioLow() {
-  document.getElementById('prioLowEdit').classList.add('prioLowGreen');
-  document.getElementById('lowImg').src = '/assets/icons/lowWhite.png';
-  document.getElementById('prioMediumEdit').classList.remove('prioMediumYellow');
-  document.getElementById('prioUrgentEdit').classList.remove('prioUrgentRed');
-  document.getElementById('mediumImg').src = '/assets/icons/mediumYellow.png';
-  document.getElementById('urgentImg').src = '/assets/icons/urgentRed.png';
+  document.getElementById("prioLowEdit").classList.add("prioLowGreen");
+  document.getElementById("lowImg").src = "/assets/icons/lowWhite.png";
+  document.getElementById("prioMediumEdit").classList.remove("prioMediumYellow");
+  document.getElementById("prioUrgentEdit").classList.remove("prioUrgentRed");
+  document.getElementById("mediumImg").src = "/assets/icons/mediumYellow.png";
+  document.getElementById("urgentImg").src = "/assets/icons/urgentRed.png";
 }
 
 function addSubtask() {
-  if (subTask.value != '') {
+  if (subTask.value != "") {
     subTasks.push({
       description: subTask.value,
       completed: false,
     });
     renderSubTaskList();
-    subTask.value = '';
+    subTask.value = "";
   }
 }
 
-subTask.addEventListener('keydown', function (event) {
-  if (event.key === 'Enter') {
+subTask.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
     event.preventDefault();
     addSubtask();
   }
 });
 
 function renderSubTaskList() {
-  let subTasksList = document.getElementById('subTaskList');
+  let subTasksList = document.getElementById("subTaskList");
 
-  subTasksList.innerHTML = '';
+  subTasksList.innerHTML = "";
 
   for (let subTasksIndex = 0; subTasksIndex < subTasks.length; subTasksIndex++) {
     subTasksList.innerHTML += generateSubTaskList(subTasksIndex);
@@ -75,22 +80,34 @@ function generateSubTaskList(subTasksIndex) {
   `;
 }
 
-async function assignedTOUsser() {
-  let user = await loadDataUsers();
-  let assignedTo = document.getElementById('contactList').classList.toggle('show');
-  assignedTo.value = user.name;
-  assignedTo.disabled = true;
-}
+// async function assignedTOUsser() {
+//   let user = await loadDataUsers();
+//   let assignedTo = document.getElementById("contactList").classList.toggle("show");
+//   assignedTo.value = user.name;
+//   assignedTo.disabled = true;
+// }
 
 window.onclick = function (event) {
-  if (!event.target.matches('.assignedTo')) {
-    let dropdowns = document.getElementsByClassName('select');
+  if (!event.target.matches(".assignedTo")) {
+    let dropdowns = document.getElementsByClassName("selecassignedTot");
     let assignedTOUsserIndex;
     for (assignedTOUsserIndex = 0; assignedTOUsserIndex < dropdowns.length; assignedTOUsserIndex++) {
       let openDropdown = dropdowns[assignedTOUsserIndex];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
+};
+
+function contactList() {
+  console.log("triger");
+  let contactList = document.getElementById("contactList");
+  contactList.innerHTML = "";
+  contacts.forEach((contact) => {
+    contactList.innerHTML += `
+      <option class="contact-list-item" onclick="selectContact(${contact.id})">${contact.name}</option>
+    `;
+  });
+  contactList.classList.toggle("show");
 }
