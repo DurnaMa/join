386171784443ddd7d8a8265715f8523b41403da6
Hardavan Alaie <h.alaie@gmx.de>
2025-02-26 20:31:34 +0100

    new page js boardPopUp

--- js/board.js
+++ js/board.js
@@ -6,8 +6,15 @@ let todos = [
     title: "Task 1",
     description: "Task 1 description",
     dueDate: "2024/10/10",
-    subTask: [{ name: "test1", completed: false }, { name: "test2", completed: false }],
-    users: [{ initial: "HA", completed: false }, { initial: "AL", completed: false }, { initial: "MD", completed: false }],
+    subTask: [
+      { name: "test1", completed: false },
+      { name: "test2", completed: false },
+    ],
+    users: [
+      { initial: "HA", completed: false },
+      { initial: "AL", completed: false },
+      { initial: "MD", completed: false },
+    ],
     prio: [],
   },
   {
@@ -17,8 +24,15 @@ let todos = [
     title: "Task 2",
     description: "Task 2 description",
     dueDate: "2024/10/15",
-    subTask: [{ name: "test3", completed: false }, { name: "test4", completed: false }],
-    users: [{ initial: "FK", completed: false }, { initial: "DL", completed: false }, { initial: "MD", completed: false }],
+    subTask: [
+      { name: "test3", completed: false },
+      { name: "test4", completed: false },
+    ],
+    users: [
+      { initial: "FK", completed: false },
+      { initial: "DL", completed: false },
+      { initial: "MD", completed: false },
+    ],
     prio: [],
   },
   {
@@ -28,8 +42,18 @@ let todos = [
     title: "Task 3",
     description: "Task 3 description",
     dueDate: "2024/10/20",
-    subTask: [{ name: "test5", completed: false }, { name: "test6", completed: false },{ name: "test6.3", completed: false },{ name: "test6.4", completed: false },{ name: "test6.5", completed: false }],
-    users: [{ initial: "HA", completed: false }, { initial: "DL", completed: false }, { initial: "RT", completed: false }],
+    subTask: [
+      { name: "test5", completed: false },
+      { name: "test6", completed: false },
+      { name: "test6.3", completed: false },
+      { name: "test6.4", completed: false },
+      { name: "test6.5", completed: false },
+    ],
+    users: [
+      { initial: "HA", completed: false },
+      { initial: "DL", completed: false },
+      { initial: "RT", completed: false },
+    ],
     prio: [],
   },
   {
@@ -39,12 +63,18 @@ let todos = [
     title: "Task 4",
     description: "Task 4 description",
     dueDate: "2024/10/25",
-    subTask: [{ name: "test7", completed: false }, { name: "test8", completed: false }],
-    users: [{ initial: "HA", completed: false }, { initial: "DL", completed: false }, { initial: "MD", completed: false }],
+    subTask: [
+      { name: "test7", completed: false },
+      { name: "test8", completed: false },
+    ],
+    users: [
+      { initial: "HA", completed: false },
+      { initial: "DL", completed: false },
+      { initial: "MD", completed: false },
+    ],
     prio: [],
-  }
+  },
 ];
-
 
 let currentDraggedElement;
 let currentSelectedTask;
@@ -64,7 +94,9 @@ function renderTasks() {
 
   todos.forEach((task) => {
     let taskCard = generateTaskCard(task);
-    let columnId = task.columnTitles ? task.columnTitles.toLowerCase().replace(" ", "") : "todo";
+    let columnId = task.columnTitles
+      ? task.columnTitles.toLowerCase().replace(" ", "")
+      : "todo";
     document.getElementById(columnId).appendChild(taskCard);
   });
 
@@ -107,9 +139,11 @@ function generateEmptyColumn(columnId) {
 }
 
 function generateTaskCard(task) {
-  let completedSubtasks = task.subTask ? task.subTask.filter(st => st.completed).length : 0;
+  let completedSubtasks = task.subTask
+    ? task.subTask.filter((st) => st.completed).length
+    : 0;
   let totalSubtasks = task.subTask ? task.subTask.length : 0;
-  
+
   let taskCard = document.createElement("div");
   taskCard.classList.add("task-card");
   taskCard.id = `task-${task.id}`;
@@ -128,10 +162,16 @@ function generateTaskCard(task) {
     
     <div class="progress-container">
         <div class="progress-bar-container">
-            <div class="progress-bar" id="progressBar-${task.id}" style="width: ${(completedSubtasks / totalSubtasks) * 100}%;"></div>
+            <div class="progress-bar" id="progressBar-${
+              task.id
+            }" style="width: ${
+    (completedSubtasks / totalSubtasks) * 100
+  }%;"></div>
         </div>
         <div class="subtasks-div">
-          <span class="subtasks-amount" id="subtasksAmount-${task.id}">${completedSubtasks}/${totalSubtasks} Subtasks</span>
+          <span class="subtasks-amount" id="subtasksAmount-${
+            task.id
+          }">${completedSubtasks}/${totalSubtasks} Subtasks</span>
         </div>
     </div>
     <div class="task-footer">    
@@ -147,30 +187,35 @@ function generateTaskCard(task) {
   `;
 
   let categoryElement = taskCard.querySelector(".task-card-category");
-    if (categoryElement) {
-      if (task.category === "User story") {
-        categoryElement.style.backgroundColor = "#0038FF";
-      } else if (task.category === "Technical task") {
-        categoryElement.style.backgroundColor = "#1FD7C1";
-      }
+  if (categoryElement) {
+    if (task.category === "User story") {
+      categoryElement.style.backgroundColor = "#0038FF";
+    } else if (task.category === "Technical task") {
+      categoryElement.style.backgroundColor = "#1FD7C1";
     }
+  }
 
   return taskCard;
 }
 
 function searchTask() {
-  let searchTaskInput = document.getElementById("searchTask").value.toLowerCase();
+  let searchTaskInput = document
+    .getElementById("searchTask")
+    .value.toLowerCase();
   let allTasks = document.querySelectorAll(".task-card");
 
-  allTasks.forEach(task => {
-      let title = task.querySelector("h3").innerText.toLowerCase();
-      let description = task.querySelector("p").innerText.toLowerCase();
+  allTasks.forEach((task) => {
+    let title = task.querySelector("h3").innerText.toLowerCase();
+    let description = task.querySelector("p").innerText.toLowerCase();
 
-      if (title.includes(searchTaskInput) || description.includes(searchTaskInput)) {
-          task.style.display = "block";  
-      } else {
-          task.style.display = "none";   
-      }
+    if (
+      title.includes(searchTaskInput) ||
+      description.includes(searchTaskInput)
+    ) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
   });
 
   searchTaskInput.innerHTML = "";
@@ -198,84 +243,6 @@ function drop(event, column) {
   }
 }
 
-function openTaskPopup(taskId) {
-  let id = todos.findIndex((task) => task.id == taskId);
-  let currentSelectedTask = todos[id];
-  
-  let openTaskPopupDiv = document.getElementById("openTaskPopupDiv");
-  openTaskPopupDiv.classList.remove("d-none");
-  openTaskPopupDiv.innerHTML = renderTasksCardPopup(currentSelectedTask);
-}
-
-function editTaskPopup(taskId) {
-  let id = todos.findIndex((task) => task.id == taskId);
-  let currentSelectedTask = todos[id];
-  
-  let editTaskPopupDiv = document.getElementById("editTaskPopupDiv");
-  let openTaskPopupDiv = document.getElementById("openTaskPopupDiv");
-  openTaskPopupDiv.classList.add("d-none");
-  editTaskPopupDiv.classList.remove("d-none");
-  editTaskPopupDiv.innerHTML = renderEditTasksCardPopup(currentSelectedTask);
-}
-
-async function editTask(taskId) {
-  let id = todos.findIndex((task) => task.id == taskId);
-  let currentSelectedTask = todos[id];
-  let task = currentSelectedTask;
-  let taskCard = document.getElementById(`task-${taskId}`);
-
-  let title = document.getElementById("titleInput").value;
-  let description = document.getElementById("descriptionTextarea").value;
-  let category = document.getElementById("category").value;
-  let dueDate = document.getElementById("date").value;
-  let priority = document.getElementById("prio").value;
-  let prioUrgentEdit = document.getElementById("prioUrgentEdit").value;
-  let prioMediumEdit = document.getElementById("prioMediumEdit").value;
-  let prioLowEdit = document.getElementById("prioLowEdit").value;
-  let subTask = document.getElementById("subTask").value;
-
-  let taskCardContent = {
-    title: title,
-    description: description,
-    category: category,
-    dueDate: dueDate,
-    priority: priority,
-    prioUrgentEdit: prioUrgentEdit,
-    prioMediumEdit: prioMediumEdit,
-    prioLowEdit: prioLowEdit,
-    subTask: subTask
-  };
-
-try {
-  await postDataToFirebase(`tasks/${taskId}`, taskCardContent);
-} catch (error) {
-  console.error(error);
-}
-
-
-  /*for (let key in taskCardContent) {
-    if (taskCardContent[key] === "") {
-      taskCardContent[key] = currentSelectedTask[key];
-  }*/
-  
-  /*currentSelectedTask.title = title;
-  currentSelectedTask.description = description;
-  currentSelectedTask.category = category;
-  currentSelectedTask.dueDate = dueDate;
-  currentSelectedTask.priority = priority;
-  currentSelectedTask.prioUrgentEdit = prioUrgentEdit;
-  currentSelectedTask.prioMediumEdit = prioMediumEdit;
-  currentSelectedTask.prioLowEdit = prioLowEdit;
-  currentSelectedTask.subTask = subTask;*/
-
-  taskCard.innerHTML = renderAddTaskPoupBtn(task);
-
-  //let editTaskPopupDiv = document.getElementById("editTaskPopupDiv");
-  //editTaskPopupDiv.classList.remove("d-none");
-
-  renderTasks();
-}
-
 function deleteTask(taskId) {
   let id = todos.findIndex((task) => task.id == taskId);
   let taskCard = document.getElementById(`task-${taskId}`);
@@ -285,12 +252,14 @@ function deleteTask(taskId) {
 }
 
 function updateSteps(taskId) {
-  let task = todos.find(t => t.id === taskId);
+  let task = todos.find((t) => t.id === taskId);
   if (!task || !task.subTask) return;
 
-  const checkboxes = document.querySelectorAll(`#taskPopUp[data-task-id='${taskId}'] .step input[type='checkbox']`);
+  const checkboxes = document.querySelectorAll(
+    `#taskPopUp[data-task-id='${taskId}'] .step input[type='checkbox']`
+  );
   let checkedCount = 0;
- 
+
   checkboxes.forEach((checkbox, index) => {
     task.subTask[index].completed = checkbox.checked;
     if (checkbox.checked) {
@@ -308,24 +277,6 @@ function updateSteps(taskId) {
   if (subtasksAmount) {
     subtasksAmount.textContent = `${checkedCount}/${checkboxes.length} Subtasks`;
   }
-}
-
-function addTaskPopupPlusToDoBtn() {
-  let openTaskPopupToDoDiv = document.getElementById("openTaskPopupToDoDiv");
-  openTaskPopupToDoDiv.classList.remove("d-none");
-  openTaskPopupToDoDiv.innerHTML = renderAddTaskPopupToDoPlus();
-}
-
-function addTaskPopupPlusInProgressBtn() {
-  let openTaskPopupInProgressDiv = document.getElementById("openTaskPopupInProgressDiv");
-  openTaskPopupInProgressDiv.classList.remove("d-none");
-  openTaskPopupInProgressDiv.innerHTML = renderAddTaskPopupInProgressPlus();
-}
-
-function addTaskPopupPlusAwaitFeedbackBtn() {
-  let openTaskPopupAwaitFeedbackDiv = document.getElementById("openTaskPopupAwaitFeedbackDiv");
-  openTaskPopupAwaitFeedbackDiv.classList.remove("d-none");
-  openTaskPopupAwaitFeedbackDiv.innerHTML = renderAddTaskPopupAwaitFeedbackPlus();
 }
 
 function createTaskPlusToDoBtn() {
@@ -388,44 +339,11 @@ function createTaskPlusAwaitFeedbackBtn() {
   closeAddTaskPopUp();
 }
 
-function closeAddTaskPopUp() {
-  document.getElementById("addNewTaskBtnDiv").classList.add("d-none");
-  document.getElementById("addTaskPopupDiv").classList.add("d-none");
-  document.getElementById("openTaskPopupDiv").classList.add("d-none");
-}
-
-function closeAddTaskPopUpToDo() {
-  document.getElementById("openTaskPopupToDoDiv").classList.add("d-none");
-}
-
-function closeAddTaskPopUpInProgress() {
-  document.getElementById("openTaskPopupInProgressDiv").classList.add("d-none");
-}
-
-function closeAddTaskPopUpAwaitFeedback() {
-  document.getElementById("openTaskPopupAwaitFeedbackDiv").classList.add("d-none");
-}
-
-function closeTaskCardPopUp() {
-  document.getElementById("openTaskPopupDiv").classList.add("d-none");
-}
-
-function closeEditTaskCardPopUp() {
-  document.getElementById("editTaskPopupDiv").classList.add("d-none");
-}
-
-function addTaskPopupBtn() {
-  let addNewTaskBtnDiv = document.getElementById("addNewTaskBtnDiv");
-  addNewTaskBtnDiv.classList.remove("d-none");
-  addNewTaskBtnDiv.innerHTML = renderAddTaskPoupBtn();
-}
-
 function createtTaskBtn() {
   let title = document.getElementById("titleInput").value;
   let description = document.getElementById("descriptionTextarea").value;
   let category = document.getElementById("category").value;
   let subTask = document.getElementById("subTask").value;
-  //let addUser = document.getElementById('')
 
   let newTask = {
     id: todos.length + 1,
@@ -440,7 +358,6 @@ function createtTaskBtn() {
 
   todos.push(newTask);
   renderTasks();
-  //closeAddTaskPopUp();
 }
 
 function addSubTaskPopUp() {
@@ -452,7 +369,7 @@ function addSubTaskPopUp() {
     subTaskPopUp.value = "";
   }
 }
-// Test 
+// Test
 
 function contactListPopUp() {
   console.log("triger");
@@ -480,4 +397,3 @@ function generateInitialsPopUp(name) {
   const lastInitial = nameParts[1]?.charAt(0) || "";
   return `${firstInitial}${lastInitial}`.toUpperCase();
 }
-
