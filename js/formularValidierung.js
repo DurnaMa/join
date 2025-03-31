e05410499623566aad83fb9321903a6d96e0ab00
Mahir Durna <159443825+DurnaMa@users.noreply.github.com>
2025-03-31 11:40:49 +0200

    red frame set

--- js/formularValidierung.js
+++ js/formularValidierung.js
@@ -1,10 +1,9 @@
 function validateForm() {
   const titleInput = document.getElementById('titleInput');
   const dateInput = document.getElementById('date');
-  const categoryDropdown = document.getElementById('dropdownCategory');
+  const categoryDropdown = document.querySelector('.categoryContainer');
   const descriptionTextarea = document.getElementById('descriptionTextarea');
-  const assignedContactsList = document.getElementById('assignedContactsList');
-
+  const assignedContactsList = document.querySelector('.assignedContainer');
   const titleError = document.getElementById('titleError');
   const dateError = document.getElementById('dateError');
   const categoryError = document.getElementById('categoryError');
@@ -20,28 +19,46 @@ function validateForm() {
   assignedContactsListError.textContent = '';
 
   if (!titleInput.value.trim()) {
-    isValid = false;
+    titleInput.classList.add('error');
     titleError.textContent = 'Title is required.';
+    isValid = false;
+  } else {
+    titleInput.classList.remove('error');
   }
 
   if (!dateInput.value.trim()) {
-    isValid = false;
+    dateInput.classList.add('error');
     dateError.textContent = 'Due Date is required.';
-  } 
+    isValid = false;
+  } else {
+    dateInput.classList.remove('error');
+  }
 
   if (!descriptionTextarea.value.trim()) {
-    isValid = false;
+    descriptionTextarea.classList.add('error');
     descriptionTextareaError.textContent = 'Description is required.';
+    isValid = false;
+  } else {
+    descriptionTextarea.classList.remove('error');
   }
 
   if (categoryDropdown.textContent.trim() === 'Select Task Category' || !categoryDropdown.textContent.trim()) {
-    isValid = false;
+    categoryDropdown.classList.add('error');
     categoryError.textContent = 'Category is required.';
+    isValid = false;
+  } else {
+    categoryDropdown.classList.remove('error');
   }
 
-  if (assignedContactsList.textContent.trim() === 'Assigned Contacts' || !assignedContactsList.textContent.trim()) {
+  if (
+    assignedContactsList.textContent.trim() === 'Select contacts to assign' ||
+    !assignedContactsList.textContent.trim()
+  ) {
+    assignedContactsList.classList.add('error');
+    assignedContactsListError.textContent = 'Assigned Contacts are required.';
     isValid = false;
-    assignedContactsListError.textContent = 'Category is required.';
+  } else {
+    assignedContactsList.classList.remove('error');
   }
 
   return isValid;
